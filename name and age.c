fa07376531bd7a6f34934dc6e3185fc31477215b
Vinuta-ashok <vinutakulenavar@gmail.com>
2025-11-17 23:05:56 +0530

    Create name and age.c

---
+++ name and age.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+char name;
+int age;
+printf("enter a name\n");
+scanf("%c\n",&name);
+printf("enter a age\n");
+scanf("%d\n",&age);
+printf("hello (name), your are (age)years old\n");
+
+}
