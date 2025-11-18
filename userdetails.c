0ff7564d5e8895709f0166291e2f639f89644c8c
Vinuta-ashok <vinutakulenavar@gmail.com>
2025-11-18 15:00:05 +0530

    Create userdetails.c

---
+++ userdetails.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+ int userdetails(){
+     printf("my name is:vinuta\n");
+     printf("my age is:18\n");
+     printf("my department is:cse\n");
+ }
+ int main(){
+   userdetails();
+ }
