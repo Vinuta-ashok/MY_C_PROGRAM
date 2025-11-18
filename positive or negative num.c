807de8606bb7cb4b113bf501c3fc69c7223e0f6f
Vinuta-ashok <vinutakulenavar@gmail.com>
2025-11-18 16:15:22 +0530

    Create positive or negative num.c

---
+++ positive or negative num.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter the value of a and b\n");
+    scanf("%d%d",&a,&b);
+    if(a>0){
+    printf("a is positive integer");}
+    else
+    printf("b is positive");
+}
