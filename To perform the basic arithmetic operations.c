5b2906aa0ff1b99703511e2ef955da419383ad94
Vinuta-ashok <vinutakulenavar@gmail.com>
2025-11-18 20:05:20 +0530

    Create To perform the basic arithmetic operations.c

---
+++ To perform the basic arithmetic operations.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int sum,diff,prod,quo,x,y;
+    printf("\n enter the values of x and y\n");
+    scanf("%d%d",&x,&y);
+    sum=x+y;
+    diff=x-y;
+    prod=x*y;
+    quo=x/y;
+    printf("sum =%d,\n difference=%d,\n,product=%d,\n quotient=%d \n",sum,diff,prod,quo);
+    
+}
